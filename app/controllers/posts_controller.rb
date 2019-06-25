bfccd34f4297109462b97df3f6d237348c7589c3
kiwasaki06 <jamjamjamjam666@gmail.com>
2019-06-25 20:52:44 +0900

    paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all.order(created_at: 'desc')
+    @posts = Post.all.order(created_at: 'desc').page(params[:page]).per(5)
   end
 
   def show
