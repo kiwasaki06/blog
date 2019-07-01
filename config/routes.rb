a86a4ab3d7f7373d09c058307795a9f905742168
kiwasaki06 <jamjamjamjam666@gmail.com>
2019-07-01 14:56:01 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/new'
   get 'login' =>  'sessions#new'
   post 'login'  =>  'sessions#create'
   delete 'logout' =>  'sessions#destroy'
   get 'signup'  =>  'users#new'
   post 'signup'  =>  'users#create'
-  resources :users
-  get 'users/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :users, only: [:create]
+  resources :users, only:[:create, :show, :edit, :update]
   resources :posts do
     resources :comments, only:[:create, :destroy]
   end
